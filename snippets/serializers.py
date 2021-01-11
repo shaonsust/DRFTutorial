d9a8ff5193f6be983d14d74ed1fe0f69db520ad9
S M Shaon <shaon63@gmail.com>
2021-01-11 16:28:52 +0600

    formatting code

--- snippets/serializers.py
+++ snippets/serializers.py
@@ -1,21 +1,32 @@
+"""
+User and Snippets model serializer.
+"""
+from django.contrib.auth.models import User
 from rest_framework import serializers
 
 from snippets.models import Snippets
 
-from django.contrib.auth.models import User
-
 
 class SnippetSerializer(serializers.HyperlinkedModelSerializer):
+    """
+    Snippet Model Serializer
+    """
     owner = serializers.ReadOnlyField(source='owner.username')
-    highlight = serializers.HyperlinkedIdentityField(view_name='snippets-highlight', format='html')
+    highlight = serializers.HyperlinkedIdentityField(
+        view_name='snippets-highlight', format='html')
 
     class Meta:
         model = Snippets
-        fields = ['url', 'id', 'highlight', 'title', 'code', 'linenos', 'language', 'style', 'owner']
+        fields = ['url', 'id', 'highlight', 'title',
+                  'code', 'linenos', 'language', 'style', 'owner']
 
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
-    snippets = serializers.HyperlinkedRelatedField(many=True, view_name='snippets-detail', read_only=True)
+    """
+    User Model Serializer.
+    """
+    snippets = serializers.HyperlinkedRelatedField(
+        many=True, view_name='snippets-detail', read_only=True)
 
     class Meta:
         model = User
